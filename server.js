5646af43403c6193bad53916b737259f9fbf8153
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-12-21 03:41:56 +0300

    modified from common js to  module

--- server.js
+++ server.js
@@ -1,36 +1,13 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
+
 const port = process.env.PORT || 8000
 
 const app = express();
 
 // app.use(express.static(path.join(__dirname, 'public')));
-
-let posts = [
-    {id:1, title: 'Post One'},
-    {id:2, title: 'Post Two'},
-    {id:3, title: 'Post Three'},
-];
-// Get all posts
-app.get('/api/posts', (req, res) => {
-    const limit = parseInt(req.query.limit);
-    if (!isNaN(limit) && limit > 0) {
-         return res.status(200).json(posts.slice(0, limit));
-    }
-    res.status(200).json(posts);
-});
-
-// Get single
-app.get('/api/posts/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const post = posts.find((post) => post.id ===id);
-
-    if (!post) {
-       return res.status(404).json({msg: `A post with the id ${id} was not found`});
-    } 
-    res.status(200).json();
-})
-
+app.use('/api/posts', posts);
 
 
 app.listen(port, () => console.log(`Server is runing on port ${port}`));
