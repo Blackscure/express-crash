182c035a0f3cc53e9679c728e2cdad7643946d0d
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-12-21 03:16:40 +0300

    modification made in sereve js

--- server.js
+++ server.js
@@ -14,12 +14,10 @@ let posts = [
 // Get all posts
 app.get('/api/posts', (req, res) => {
     const limit = parseInt(req.query.limit);
-
     if (!isNaN(limit) && limit > 0) {
-        res.status(200).json(posts.slice(0, limit));
-    } else {
-        res.status(200).json(posts);
+         return res.status(200).json(posts.slice(0, limit));
     }
+    res.status(200).json(posts);
 });
 
 // Get single
@@ -28,10 +26,9 @@ app.get('/api/posts/:id', (req, res) => {
     const post = posts.find((post) => post.id ===id);
 
     if (!post) {
-        res.status(404).json({msg: `A post with the id ${id} was not found`});
-    } else {
-        res.status(200).json();
-    }
+       return res.status(404).json({msg: `A post with the id ${id} was not found`});
+    } 
+    res.status(200).json();
 })
 
 
